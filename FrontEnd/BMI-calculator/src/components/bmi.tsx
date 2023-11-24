69d3fb8d706229669103e90a53069faf5395b735
Omkar Patil <git21cs072-t@students.git.edu>
2023-11-24 20:50:12 +0530

    all State Variables Working as expected

--- FrontEnd/BMI-calculator/src/components/bmi.tsx
+++ FrontEnd/BMI-calculator/src/components/bmi.tsx
@@ -3,22 +3,28 @@ import { useState } from "react";
 
 
 function BMI(){
-    const[height,setHeight] = useState(null);
-    const[weight,setWeight] = useState(null);
+    const[height,setHeight] = useState<number|undefined>(undefined);
+    const[weight,setWeight] = useState<number|undefined>(undefined);
     return<div style={ {display:'flex',
     justifyContent:'center',
     flexWrap:'wrap'}}>
         <Card style={{width:400,padding:20,marginTop:10}}>
             <Typography variant="h6" marginBottom={2}>Enter Your Details</Typography>
-            <TextField placeholder="Height"></TextField>
+            <TextField placeholder="Height" onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+               const newHeight = parseFloat(e.target.value);
+               setHeight(isNaN(newHeight) ? undefined : newHeight);
+            }}></TextField>
             <br></br>
             
-            <TextField placeholder="Weight" style={{marginTop:10}}></TextField>
+            <TextField placeholder="Weight" style={{marginTop:10}} onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+               const newHeight = parseFloat(e.target.value);
+               setHeight(isNaN(newHeight) ? undefined : newHeight);
+            }}></TextField>
             <br />
-            <Button variant="contained" size="large" style={{marginTop:10}} >Calculate BMI</Button>
+            <Button variant="contained" size="large" style={{marginTop:10}} onClick={} >Calculate BMI</Button>
 
         </Card>
-    hellow world
+    
     </div>
 }
 
