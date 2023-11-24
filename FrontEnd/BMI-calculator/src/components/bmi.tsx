5f9f554abe9f7346d182cb7e4830c877f7d5f825
Omkar Patil <git21cs072-t@students.git.edu>
2023-11-24 20:43:47 +0530

    added height and weight State VARS

--- FrontEnd/BMI-calculator/src/components/bmi.tsx
+++ FrontEnd/BMI-calculator/src/components/bmi.tsx
@@ -1,18 +1,21 @@
 import { Button, Card, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 
 function BMI(){
+    const[height,setHeight] = useState(null);
+    const[weight,setWeight] = useState(null);
     return<div style={ {display:'flex',
     justifyContent:'center',
     flexWrap:'wrap'}}>
-        <Card style={{width:400,padding:20}}>
-            <Typography>Enter Your Details</Typography>
+        <Card style={{width:400,padding:20,marginTop:10}}>
+            <Typography variant="h6" marginBottom={2}>Enter Your Details</Typography>
             <TextField placeholder="Height"></TextField>
             <br></br>
             
             <TextField placeholder="Weight" style={{marginTop:10}}></TextField>
             <br />
-            <Button size="large">Calculate BMI</Button>
+            <Button variant="contained" size="large" style={{marginTop:10}} >Calculate BMI</Button>
 
         </Card>
     hellow world
