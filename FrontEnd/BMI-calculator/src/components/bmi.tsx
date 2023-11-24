4ee3b6c66ae3709d9dc563cf461421fed071dd24
Omkar Patil <git21cs072-t@students.git.edu>
2023-11-24 20:38:10 +0530

    BMI Function half written

--- FrontEnd/BMI-calculator/src/components/bmi.tsx
+++ FrontEnd/BMI-calculator/src/components/bmi.tsx
@@ -1,9 +1,22 @@
+import { Button, Card, TextField, Typography } from "@mui/material";
 
 
 function BMI(){
-    return<>
+    return<div style={ {display:'flex',
+    justifyContent:'center',
+    flexWrap:'wrap'}}>
+        <Card style={{width:400,padding:20}}>
+            <Typography>Enter Your Details</Typography>
+            <TextField placeholder="Height"></TextField>
+            <br></br>
+            
+            <TextField placeholder="Weight" style={{marginTop:10}}></TextField>
+            <br />
+            <Button size="large">Calculate BMI</Button>
+
+        </Card>
     hellow world
-    </>
+    </div>
 }
 
 export default BMI;
